0fe46eaaab575811cafcb81ccf0b802bd83a7c17
BadAssWalrus <jackyPhamofficial@gmail.com>
2016-07-19 03:35:43 -0700

    Initial commit

---
+++ src/Concept.java
@@ -0,0 +1,16 @@
+public class Concept
+{
+	
+	private String hair_style = 0, hat_style = 0;
+	private int hair_color = 0, hat_color = 0, clothing_color = 0;
+	private double monster_food = 70, monster_happiness = 70, monster_affection = 70, monster_energy = 100, monster_health = 100;
+	private boolean ifFed = false;
+
+	
+
+	public checkHappiness()
+	{
+		if ((monster_food < 50 || monster_health < 50) || (monster_food < 50 && monster_health < 50))
+	}
+	
+}//end of class Concept
